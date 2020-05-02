ab38f72145e53905f41cb55e6136ed089a8f940f
Haoyu Li <haoyu.li@sjtu.edu.cn>
2020-05-02 13:04:52 +0800

    implement IPC

--- ShimLayer.c
+++ ShimLayer.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
 #include "ShimLayer.h"
+#include <stdlib.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#include <semaphore.h>
+#include <sys/wait.h>
+
+#define SIZE 1024
+#define JNI_NUM 7
 
 JNIEXPORT void JNICALL Java_ShimLayer_run(JNIEnv *env, jclass jc) {
-    printf("Hello Native\n");
+  sem_t* sem_id_jvm = sem_open("/sem-jvm", O_CREAT, 0666, 0);
+  if (sem_id_jvm < SEM_FAILED) {
+    perror("Reader: sem_open failed!\n");
+  }
+
+  sem_t* sem_id_container = sem_open("/sem-container", O_CREAT, 0666, 0);
+  if (sem_id_container < SEM_FAILED) {
+    perror("Reader: sem_open failed!\n");
+  }
+
+  int fd = shm_open("/shmem", O_CREAT | O_RDWR, 0666);
+  if (fd < 0) {
+    perror("Reader: shm_open failed!\n");
+    return EXIT_FAILURE;
+  }
+
+  char *data =
+      (char *)mmap(0, SIZE, PROT_READ, MAP_SHARED, fd, 0);
+
+  for (int i = 0; i < JNI_NUM; i++) {    
+    if (sem_wait(sem_id_jvm) < 0) {
+      perror("Reader: sem_wait failed!\n");
+    }
+
+    printf("Received: %s\n", data);
+
+    if (sem_post(sem_id_container) < 0) {
+      perror("Reader: sem_post failed!\n");
+    }
+  }
+
+  if (sem_close(sem_id_jvm) != 0) {
+    perror("Writer: sem_close failed!\n");
+  }
+
+  munmap(data, SIZE);
+
+  close(fd);
+
+  return EXIT_SUCCESS;
 }
