d4e212164283321fc68902649214d0c1ff0f5e9d
Haoyu Li <haoyu.li@sjtu.edu.cn>
2020-05-14 19:23:38 +0800

    suit for docker container

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-/build/linux-x86_64-normal-server-release/jdk/bin/javac ShimLayer
+/build/linux-x86_64-normal-server-release/jdk/bin/javac ShimLayer.java
 gcc -o init-sem init-sem.c -lpthread -lrt
 gcc -o libShimLayer.so ShimLayer.c -lpthread -lrt -lJMagick -fPIC -shared -I/build/linux-x86_64-normal-server-release/jdk/include/ -I/build/linux-x86_64-normal-server-release/jdk/include/linux/
-sudo cp libShimLayer.so /usr/lib/
+cp libShimLayer.so /usr/lib/
