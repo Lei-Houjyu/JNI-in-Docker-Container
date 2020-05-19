b0735f5e7223867a19e131690aa08011d9133b49
Haoyu Li <haoyu.li@sjtu.edu.cn>
2020-05-19 16:57:07 +0800

    print to output.txt

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 # echo "initializing semaphores..."
 # ./init-sem $1
 # echo "running..."
-/build/linux-x86_64-normal-server-release/jdk/bin/java ShimLayer $1
+/build/linux-x86_64-normal-server-release/jdk/bin/java ShimLayer $1 > output.txt
 # echo "finish!"
 
